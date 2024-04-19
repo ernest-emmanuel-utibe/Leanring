fcba2e35a028d98a1ce210dca2ea93dda5e2bcd8
Ernest Emmanuel Utibe <em123nest@gmail.com>
2024-04-19 15:29:51 +0100

    Update LearningdemoApplication.java

--- src/main/java/com/example/learningdemo/LearningdemoApplication.java
+++ src/main/java/com/example/learningdemo/LearningdemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearningdemoApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LearningdemoApplication.class, args);
 	}
